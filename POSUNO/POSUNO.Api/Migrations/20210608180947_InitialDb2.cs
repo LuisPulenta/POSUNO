57338d76742316e3bb3291d1b377a73c148a013b
Luis Núñez <luisalbertonu@gmail.com>
2021-06-09 11:42:37 -0300

    Hasta aca llegue

---
+++ POSUNO/POSUNO.Api/Migrations/20210608180947_InitialDb2.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace POSUNO.Api.Migrations
+{
+    public partial class InitialDb2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Users_UserId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Users_UserId",
+                table: "Products",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Users_UserId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Users_UserId",
+                table: "Products",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
